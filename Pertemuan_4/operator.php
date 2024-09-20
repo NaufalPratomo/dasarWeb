b178babe5132eb6342f7e23b40aa8037b962bbb1
NaufalPratomo <naufalpratomo8@gmail.com>
2024-09-20 08:33:43 +0700

    add: soal no.3.1

---
+++ Pertemuan_4/operator.php
@@ -0,0 +1,18 @@
+<?php
+$a = 10;
+$b = 5;
+
+$hasilTambah = $a + $b;
+$hasilKurang = $a - $b;
+$hasilKali = $a * $b;
+$hasilBagi = $a / $b;
+$sisaBagi = $a % $b;
+$pangkat = $a ** $b;
+
+echo "Hasil Penjumlahan: " . $hasilTambah . "<br>";
+echo "Hasil Pengurangan: " . $hasilKurang . "<br>";
+echo "Hasil Perkalian: " . $hasilKali . "<br>";
+echo "Hasil Pembagian: " . $hasilBagi . "<br>";
+echo "Sisa Bagi: " . $sisaBagi . "<br>";
+echo "Hasil Pangkat: " . $pangkat . "<br>";
+?>
