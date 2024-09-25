163a7514724dea3526945ea686c6ee1ca152a7b6
NaufalPratomo <naufalpratomo8@gmail.com>
2024-09-25 20:40:00 +0700

    add: no 12

--- Pertemuan_5/string3.php
+++ Pertemuan_5/string3.php
@@ -1,6 +1,11 @@
 <?php
+$pesan = "saya arek malang";
+# ubah variabel $pesan menjadi array dengan perintah explode
+$pesanPerkata = explode(" ", $pesan);
+# ubah setiap kata dalam array menjadi kebalikannya
+$pesanPerkata = array_map(fn($pesan) => strrev($pesan), $pesanPerkata);
+# gabungkan kembali array menjadi string
+$pesan = implode("", $pesanPerkata);
 
-$pesan = "Saya arek malang";
-echo strrev($pesan) . "<br>";
-
+echo $pesan. "<br>";
 ?>
