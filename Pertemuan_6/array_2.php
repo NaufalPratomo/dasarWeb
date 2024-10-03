6983f9e10a032711986643566f041c01fd185e9f
NaufalPratomo <naufalpratomo8@gmail.com>
2024-10-03 23:08:37 +0700

    add: praktikum 3 soal 3

--- Pertemuan_6/array_2.php
+++ Pertemuan_6/array_2.php
@@ -3,8 +3,36 @@
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title></title>
+    <title>Informasi Dosen</title>
+    <style>
+        h1 {
+            color: white;
+        }
+
+        body {
+            background-image: url('https://media.discordapp.net/attachments/1146001266300964953/1291427007405490176/4k-lonely-japanese-city_bmZobWWUmZqaraWkpJRobWllrWdma2o.png?ex=67000ea0&is=66febd20&hm=60e108f3ee170c63f9d9fb168208e8ce1342eb81970bcdcef57a841c506d05e0&=&format=webp&quality=lossless&width=1089&height=614');
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+        }
+
+        table {
+            border-collapse: collapse;
+            width: 50%;
+            margin: auto;
+            background-color: rgba(255, 255, 255, 0.8);
+        }
+
+        th, td {
+            border: 1px solid #ddd;
+            padding: 8px;
+            text-align: left;
+        }
+
+        th {
+            background-color: #f2f2f2;
+        }
+    </style>
 </head>
 
 <body>
@@ -14,10 +42,27 @@
         'domisili' => 'Malang',
         'jenis kelamin' => 'Perempuan'
     ];
-    echo "Nama : {$Dosen['nama']} <br>";
-    echo "Domisili : {$Dosen['domisili']} <br>";
-    echo "Jenis Kelamin : {$Dosen['jenis kelamin']} <br>";
     ?>
+
+    <h1>Informasi Dosen</h1>
+    <table>
+        <tr>
+            <th>Attribut</th>
+            <th>Nilai</th>
+        </tr>
+        <tr>
+            <td>Nama</td>
+            <td><?= $Dosen['nama'] ?></td>
+        </tr>
+        <tr>
+            <td>Domisili</td>
+            <td><?= $Dosen['domisili'] ?></td>
+        </tr>
+        <tr>
+            <td>Jenis Kelamin</td>
+            <td><?= $Dosen['jenis kelamin'] ?></td>
+        </tr>
+    </table>
 </body>
 
 </html>
