866ce308ec2c090b5babbd3a3d48ea2c4054d8e1
NaufalPratomo <naufalpratomo8@gmail.com>
2024-10-11 10:31:41 +0700

    add: praktikum 5 soal 5.4

--- Pertemuan_7/regex.php
+++ Pertemuan_7/regex.php
@@ -21,4 +21,13 @@ $replacement = 'banana';
 $text = 'I like apple pie. ';
 $new_text = preg_replace($pattern, $replacement, $text);
 echo $new_text . "<br>"; // Output: "I like banana pie."
+
+$pattern = '/go*d/'; // Cocokkan "god", "good", "gooood", dll.
+$text = 'god is good.';
+if (preg_match($pattern, $text, $matches)) {
+echo "Cocokkan: " . $matches[0] . "<br>";
+} else {
+    echo "Tidak ada yang cocok! <br>";
+}
 ?>
+
