9145822571d18e2c2b9e7e83439d0052bb59c1dd
NaufalPratomo <naufalpratomo8@gmail.com>
2024-10-17 22:09:13 +0700

    add: percobaan 3 soal no. 11

--- Pertemuan_8/upload_ajax.php
+++ Pertemuan_8/upload_ajax.php
@@ -1,25 +1,28 @@
 <?php
-if (isset($_FILES['file'])) {
+if (isset($_FILES['files'])) {
     $errors = array();
-    $file_name = $_FILES['file']['name'];
-    $file_size = $_FILES['file']['size'];
-    $file_tmp = $_FILES['file']['tmp_name'];
-    $file_type = $_FILES['file']['type'];
-    @$file_ext = strtolower("" . end(explode('.', $_FILES['file']['name'])) . "");
-    $extensions = array("pdf", "doc", "docx", "txt");
+    $allowed_extensions = array("jpg", "jpeg", "png", "gif");
 
-    if (in_array($file_ext, $extensions) === false) {
-        $errors[] = "Ekstensi file yang diizinkan adalah PDF, DOC, DOCX, atau TXT.";
-    }
+    foreach ($_FILES['files']['tmp_name'] as $key => $tmp_name) {
+        $file_name = $_FILES['files']['name'][$key];
+        $file_size = $_FILES['files']['size'][$key];
+        $file_tmp = $_FILES['files']['tmp_name'][$key];
+        $file_ext = strtolower(pathinfo($file_name, PATHINFO_EXTENSION));
 
-    if ($file_size > 2097152) {
-        $errors[] = 'Ukuran file tidak boleh lebih dari 2 MB';
-    }
+        if (!in_array($file_ext, $allowed_extensions)) {
+            $errors[] = "$file_name: Ekstensi file yang diizinkan adalah JPG, JPEG, PNG, atau GIF.";
+        }
 
-    if (empty($errors) == true) {
-        move_uploaded_file($file_tmp, "documents/" . $file_name);
-        echo "File berhasil diunggah.";
-    } else {
-        echo implode(" ", $errors);
+        if ($file_size > 2097152) {
+            $errors[] = "$file_name: Ukuran file tidak boleh lebih dari 2 MB.";
+        }
+
+        if (empty($errors)) {
+            move_uploaded_file($file_tmp, "documents/" . $file_name);
+            echo "$file_name berhasil diunggah.<br>";
+        } else {
+            echo implode(" ", $errors) . "<br>";
+        }
     }
 }
+?>
