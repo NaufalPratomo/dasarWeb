7aa03f3dbd677ba748debd7ca48ec373d4d111ee
NaufalPratomo <naufalpratomo8@gmail.com>
2024-10-17 20:12:52 +0700

    add: praktikum 1 soal no.5

--- Pertemuan_8/upload.php
+++ Pertemuan_8/upload.php
@@ -1,6 +1,6 @@
 <?php
 if (isset($_POST["submit"])) {
-    $targetdir = "uploads/"; //Direktori tujuan untuk menyimpan file
+    $targetdir = "uploads/"; // Direktori tujuan untuk menyimpan file
     $targetfile = $targetdir . basename($_FILES["myfile"]["name"]);
     $fileType = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION));
 
@@ -9,7 +9,8 @@ if (isset($_POST["submit"])) {
     
     if (in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"] <= $maxsize) {
         if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)) {
-            echo "File berhasil diunggah.";
+            echo "File berhasil diunggah.<br>";
+            echo "<img src='$targetfile' width='200' style='height: auto;'><br>";
         } else {
             echo "Gagal mengunggah file.";
         }
